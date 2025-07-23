cf203b01accebd3abea71f6b8b510150bd6e5a39
Chong <chongjinjye@gmail.com>
2025-07-23 13:46:24 +0800

    added new SQL page

--- projectcomponents.py
+++ projectcomponents.py
@@ -243,6 +243,52 @@ projects = {
                     """
                 },
                 "project7": {
+                    "title": "Data Engineer Jobs Analysis",
+                    "tools_used": ["SQL"],
+                    "additional_skills": ["PostgreSQL", "SQLTools (extension)", "Plotly"],
+                    "icon": "📊",
+                    "link": "pages/SQLLukeBarousse.py",
+                    "main_image": "assets/sqllb0.png",
+                    "description": """
+                    The project aims to provide insights into the data engineering job market using data visualizations.
+                    The project showcases intermediate skills in SQL and Python, using PostgreSQL as the database and VS Code with SQLTools extension for database management.
+                    Partially guided by Luke Barousse's SQL for Data Analytics, the project is mostly my own work. 
+                    """,
+                    "resources": """
+                    Link to GitHub: https://github.com/JYNgithub/Project-SQL-LukeBarousse
+                    """,
+                    "add_image1": "assets/sqllb1.png",
+                    "add_image2": "assets/sqllb2.png",
+                    "add_image3": "assets/sqllb3.png",
+                    "add_image4": "assets/sqllb4.png",
+                    "documentation": """
+
+                    **Workflow**\n
+                    The entire workflow is executed in VS code using the 'SQLTools' extension to connect to PostgreSQL.
+                    1. The database and tables are first created using SQL in the 'sql_load' folder.
+                    2. The database from PostgreSQL is connected to VS Code using the SQLTools extension.
+                    3. The data is queried using SQL in the 'sql_queries' folder for concise data for analysis and visualizations.
+                    4. Queried data are converted to CSV files temporarily stored in 'queried_csv' folder.
+                    5. The data is visualized using the Python library Plotly in the 'visualizations' folder.
+
+                    **Findings**\n
+                    1. What is the typical hourly pay rate for a Data Engineer?
+                    A majority of Data Engineer roles pay for 52.5 - 57.5 USD per hour, followed by 57.5 - 62.5 and 62.5 - 67.5. 
+                    This is much higher than the average hourly pay in the US of 28.2 - 35.5 USD according to news sources.
+                    
+                    2. What are the top-paying jobs available for Data Engineers?
+                    Top paying jobs for Data Engineer roles can reach up to 117.50 USD per hour. Furthermore, half of the top paying jobs for Data Engineer roles does not specify requiring a degree in their job postings.
+                    
+                    3. What are the top skill types based on salary for Data Engineers?
+                    To nobody's surprise, SQL and Python remain the most demanded skills among Data Engineer roles, which are fundamentals for building data pipelines and ensuring data integrity in any organization. Modern platforms with database services such as AWS and Azure should also be noted.
+                    
+                    4. What are the most high-demand AND high-paying skills in the field of Data Engineering?
+                    It is observed that SQL and Python are the most demanded skills, but they are not the highest-paying skills.
+                    In terms of demand, Azure and Spark are almost equal, but Spark offers more opportunities for higher paying jobs.
+                    Another interesting point is Databricks, which is the highest paying skill yet low in demand. This could be because Databricks is still a new skill in the market, but is definitely acknowledge as employers are willing to pay a high amount for this expertise.
+                    """
+                },
+                "project8": {
                     "title": "Sustainable City Dashboard",
                     "tools_used": ["R"],
                     "additional_skills": ["Rshiny", "Linear Regression", "Forecasting"],
